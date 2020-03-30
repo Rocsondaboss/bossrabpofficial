a200585f4828d21c3aafbaab6e003c50a0a48c9b
Rocsondaboss <61299589+Rocsondaboss@users.noreply.github.com>
2020-03-30 14:33:10 +0800

    Add files via upload

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,13 +1,14 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand text-white" href="#"><img src="img/rabp logo.png" alt="" width="134px" height="48.66"></a>
+            <a class="navbar-brand text-white" href="index.php"><img src="img/rabp logo.png" alt="" width="134px" height="48.66"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <!---
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link text-white" href="#">Home</a>
+                        <a class="nav-link text-white" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-white" href="#">About Us</a>
@@ -22,6 +23,7 @@
                         <a class="nav-link text-white" href="#">Contact</a>
                     </li>
                 </ul>
+                --->
             </div>
         </div>
     </nav>
