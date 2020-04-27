f49bf60238b6107e9d87dcab12a73a523dd6ae40
Rocsondaboss <61299589+Rocsondaboss@users.noreply.github.com>
2020-04-27 21:12:35 +0800

    Add files via upload

---
+++ watch-top-funny-videos-you-want-to-watch-on-youtube-2.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>The bashing against Duterte (January-April 2020)</title>
+</head>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link href="mystyle.css" rel="stylesheet">
+
+<body>
+
+
+
+<!------ Include the above in your HEAD tag ---------->
+
+
+    <!-- Navigation -->
+   <?php include 'includes/navbar.php';?>
+    <!-- Navigation -->
+
+
+    <!-- Carousel Slider -->
+    <div id="carouselLogo" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselLogo" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselLogo" data-slide-to="1"></li>
+            <li data-target="#carouselLogo" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <img class="d-block img-fluid" src="http://www.facenet.org/wp-content/themes/executive/assets/images/header.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="http://www.facenet.org/wp-content/themes/executive/assets/images/header.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="http://www.facenet.org/wp-content/themes/executive/assets/images/header.jpg" alt="First slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselLogo" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselLogo" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <!-- Carousel Slider -->
+
+    <!-- Card -->
+    <!-- Card -->
+    <div class="container container mt-4 mb-9" style="margin-top: 25px;">
+        <hr class="bg-dark mb-4 w-25" style="margin-top: 85px;">
+        <h1 style="font-size:50px;">WATCH: Top funny videos you want to watch on Youtube</h1>
+        <h1 style="font-size:16px;">Posted on April 12, 2020</h1>
+
+        <h5>21. "Million fans around the world" / Walang aawat ha"</h5>
+        <figure>
+             <iframe width="560" height="315" src="https://www.youtube.com/embed/BvnV9vHrxRc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+             <figcaption>Note: This is not the original video of the "walang aawat ha". Instead the "walang aawat" audio was inserted over the video.</figcaption>
+        </figure>
+
+        <p>The original video with the audio "million fans around the world" and walang aawat" was nowhere to be found on the internet (even searching on YouTube). However, the above-mentioned audio clip was commonly used in fight-theme videos, like this example above.</p>
+
+        <h5>22. Snake barges at the internet shop</h5>
+        <figure>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/XTZymwd1LSE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </figure>
+        <p>Captured on CCTV cameras dated April 8, 2017, we see inside the internet shop located in Wiset Chai Chan District, Ang Thong, Thailand where the unsuspected teenage boy walking out of the door; as the boy opens it, the snake lunges into the door and appears to cling at his waist. The boy slips in panic and quickly runs out of another door, causing chaos inside the café. Luckily, according to <a href="https://www.youtube.com/watch?v=wtGdyFSAAfE">TomoNews US</a>, the type of the snake shown is the rat snake, which is belong to nonvenomous snake families.</p>
+
+       
+       <h5 style="text-align: center"><a href="watch-top-funny-videos-you-want-to-watch-on-youtube.php"><<< Previous page</a></h5>
+
+       <h5>23. Duterte dancing to budots</h5>
+        <figure>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/kRv7gIRpaC4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </figure>
+        <p>This 2016 video showing Rodrigo Duterte, who was the Davao City Mayor at the time, along with the local residents dancing the "budots", a music genre that originated in Visayas.</p>
+
+                </div>
+            </div>
+            
+            </div>
+        </div>
+    </div>
+    <!-- Card -->
+    <!-- Card -->
+
+    <!-- Pagination -->
+
+      <!-- Pagination -->
+
+    <!-- Footer -->
+    <footer class="footer bg-dark text-white">
+
+        <!-- Social Icons -->
+        <div class="bg-primary">
+            <div class="container">
+                <div class="row py-4">
+                    <div class="col-md-6 col-lg-7">
+                        <h4 class="mb-0 white-text">Get connected with me on social networks!</h4>
+                    </div>
+                    <div class="col-md-6 col-lg-5 text-right">
+                        
+                        <a><img src="img/facebook.png" width="16px;" height="16px;" alt="Italian Trulli"> </a>
+                        <a><img src="img/instagram.png" width="16px;" height="16px;" alt="Italian Trulli"> </a>
+                        <a><img src="img/174876.svg" width="16px;" height="16px;" alt="Italian Trulli"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Social Icons -->
+
+        <!-- Footer Links -->
+        <div class="container pt-5 pb-2">
+            <div class="row">
+
+                <div class="col-md-3 col-lg-4 col-xl-3">
+                    <h4>RABP</h4>
+                    <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                    <p>
+                       Testing.
+                    </p>
+                </div>
+
+               
+<!---
+                <div class="col-md-4 col-lg-3 col-xl-3">
+                    <h4>Contact</h4>
+                    <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                    <p><i class="fa fa-home mr-3"></i> Company Location</p>
+                    <p><i class="fa fa-envelope mr-3"></i> info@example.com</p>
+                    <p><i class="fa fa-phone mr-3"></i> + 98 765 432 11</p>
+                    <p><i class="fa fa-print mr-3"></i> + 98 765 432 10</p>
+                </div>
+                --->
+
+            </div>
+        </div>
+        <!-- Footer Links-->
+
+        <hr class="bg-white mx-4 mt-2 mb-1">
+
+        <!-- Copyright-->
+        <div class="container-fluid">
+            <p class="text-center m-0 py-1">
+                © 2020 Copyright BossRA
+            </p>
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+
+</body>
+</html>
